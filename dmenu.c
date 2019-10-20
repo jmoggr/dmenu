e6292b62bbdd52d630e84d481bec8c5e3f369fd0
Jason Moggridge <jasonmoggridge@gmail.com>
2019-10-20 16:03:22 -0400

    Show current selection in quick select mode
    The user can still select items and navigate the list using shortcuts,
    showing the cursor is necessary for this. It was originally hidden in
    quick select mode for aesthetic reasons.

--- dmenu.c
+++ dmenu.c
@@ -117,7 +117,7 @@ cistrstr(const char *s, const char *sub)
 static int
 drawitem(struct item *item, int x, int y, int w)
 {
-	if (item == sel && !quick_select)
+	if (item == sel)
 		drw_setscheme(drw, scheme[SchemeSel]);
 	else if (item->out)
 		drw_setscheme(drw, scheme[SchemeOut]);
@@ -141,11 +141,13 @@ drawmenu(void)
 		drw_setscheme(drw, scheme[SchemeSel]);
 		x = drw_text(drw, x, 0, promptw, bh, lrpad / 2, prompt, 0);
 	}
+
 	/* draw input field */
 	w = (lines > 0 || !matches) ? mw - x : inputw;
 	drw_setscheme(drw, scheme[SchemeNorm]);
 	drw_text(drw, x, 0, w, bh, lrpad / 2, text, 0);
 
+	/* draw input field cursor cursor */
 	if (!quick_select) {
 		curpos = TEXTW(text) - TEXTW(&text[cursor]);
 		if ((curpos += lrpad / 2 - 1) < w) {
